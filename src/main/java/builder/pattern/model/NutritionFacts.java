e6d16abfeed2c0c03e2eaf5cfab852d6f9cf8ae4
Arthur Schneider <dev.schneiart@gmail.com>
2018-02-20 21:55:53 +0100

    Add builder pattern example

---
+++ src/main/java/builder/pattern/model/NutritionFacts.java
@@ -0,0 +1,72 @@
+package builder.pattern.model;
+
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters - initialized to default values
+        private int calories     = 0;
+        private int fat          = 0;
+        private int sodium       = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "The ServingSize is " + servingSize + "\n" 
+                + "and can be served " + servings + " times a day.\n"
+                + " Calories: " + calories + "mg\n"
+                + " Fat: " + fat + "mg\n"
+                + " Sodium: " + sodium + "mg\n"
+                + " Carbohydrate: " + carbohydrate + "mg";
+    }
+
+}
